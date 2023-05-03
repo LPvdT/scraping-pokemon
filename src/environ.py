f09538915f8c7108695406c16accfc96d247727b
Laurens van der Tas <laurens.vandertas@gmail.com>
2023-05-03 14:19:37 +0200

    Change: Restructure - expanded environ

--- src/environ.py
+++ src/environ.py
@@ -1,5 +1,28 @@
 from pathlib import Path
+from urllib.parse import urljoin
 
+from rich.console import Console
+
+from src.types import FirefoxParams
+
+
+# Setup
+CONSOLE = Console(record=True, tab_size=4)
+
+# Switches
+FIREFOX_PARAMS = FirefoxParams(
+    headless=False,
+)
+KEEP_ALIVE = False
+LIMIT_POKEDEX = 1
+LIMIT_CARDS = 5
+
+# URL
+URL_ROOT = "https://pokemondb.net"
+URL_POKEDEX_INDEX = "pokedex"
+ENTRYPOINT: str = urljoin(URL_ROOT, URL_POKEDEX_INDEX)
+
+# Structure
 FOLDERS: list[str] = ["data/static/img", "data/static/logs"]
 
 
