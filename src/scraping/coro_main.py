f2924a322b9daf8aaed4dced08879e4d40898c04
Laurens van der Tas <laurens.vandertas@gmail.com>
2023-05-03 15:56:06 +0200

    Change: Add coro_main dumps

--- src/scraping/coro_main.py
+++ src/scraping/coro_main.py
@@ -20,22 +20,11 @@ async def main_coroutine(
     # Follow entrypoint URL
     page: Page = await utils.navigate(url=ENTRYPOINT, browser=browser)
 
-    # Show title
-    await utils.dump_console_recording(
-        CONSOLE, title="root_title", type="svg"
-    )
-
     # Get Pokédex URLs
     urls_pokedex: List[str] = await get_pokedex_urls(page)
-    await utils.dump_console_recording(
-        CONSOLE, title="urls_pokedex", type="svg"
-    )
 
     # Get generation URLs
     _: List[str] = await get_generation_urls(page, urls_pokedex)
-    await utils.dump_console_recording(
-        CONSOLE, title="urls_generations", type="svg"
-    )
 
     # Data Pokédex cards
     (
@@ -43,9 +32,20 @@ async def main_coroutine(
         data_pokedex_cards_data,
     ) = await get_pokedex_cards(page, urls_pokedex)
 
+    await utils.save_json(
+        data_pokedex_cards_data, "data_pokedex_cards_data"
+    )
+
     # Get Pokémon details
-    # TODO: Return shit from the coroutine
-    await get_pokemon_details(page, data_pokedex_cards_img)
+    data_pokemon_details = await get_pokemon_details(
+        page, data_pokedex_cards_img
+    )
+
+    await utils.save_json(data_pokemon_details, "data_pokemon_details")
+
+    await utils.dump_console_recording(
+        console=CONSOLE, title="recording", type="svg"
+    )
 
     # Teardown
     await utils.teardown(browser)
