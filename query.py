525bf2837bbaebe677f46eef62505b07df2987a9
Laurens van der Tas <laurens.vandertas@gmail.com>
2023-05-10 18:34:06 +0200

    Change: Janky query entrypoint

--- query.py
+++ query.py
@@ -1,7 +1,11 @@
 from src.database.db import db
+import pandas as pd
 from tinydb import Query
 
 pokemon = Query()
-db.table("pokemon").search(pokemon.name == "Bulbasaur")
+
+res = db.table("pokemon").all()
+
+pd.json_normalize(res)
 
 db.table("cards_img").all()
