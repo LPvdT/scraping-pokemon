06a55e73d84c4422ff3e10536db82ed27c12549a
Laurens van der Tas <laurens.vandertas@gmail.com>
2023-05-13 18:27:13 +0200

    Change: Clean up query

--- scraping_pokemon/query.py
+++ scraping_pokemon/query.py
@@ -1,16 +1,13 @@
 from .src.database.db import db
 import pandas as pd
-from tinydb import Query
 
 
 def run() -> None:
-    pokemon = Query()
-
     res = db.table("pokemon").all()
-
-    print(len(res))
-    # pd.json_normalize(res)
-
     res_cards = db.table("cards_img").all()
 
+    print(len(res))
+    print(pd.json_normalize(res).head(25))
+
     print(len(res_cards))
+    print(pd.json_normalize(res_cards).head(25))
